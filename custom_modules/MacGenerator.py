c5315e2657964f6fa463ae38e0bde1adc25d7e89
= <quobod@gmail.com>
2022-08-12 01:45:51 -0400

    Added module

---
+++ custom_modules/MacGenerator.py
@@ -0,0 +1,16 @@
+import random
+
+
+def random_mac():
+    return [
+        0x00,
+        0x50,
+        0x56,
+        random.randint(0x00, 0x7F),
+        random.randint(0x00, 0xFF),
+        random.randint(0x00, 0xFF),
+    ]
+
+
+def mac_pretty_print(mac):
+    return ":".join(map(lambda x: "%02x" % mac))

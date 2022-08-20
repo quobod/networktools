f7ff80d58ee3eebb799332a4da4ee12084fd24f0
= <quobod@gmail.com>
2022-08-20 12:09:53 -0400

    Renamed file to match module

--- arpcommandertests.py
+++ arpcommandertests.py
@@ -2,7 +2,7 @@
 
 from custom_modules.ArgumentManager import filtered, filtered_count
 from custom_modules.ConsoleMessenger import CONSOLE_MESSENGER_SWITCH as cms
-from custom_modules.LocalConfigParser import (
+from custom_modules.ArpCommander import (
     get_network_interface_hardware_address as mac,
     get_network_interface_name as iface_name,
     get_routing_table as routing,
@@ -12,4 +12,6 @@ print("MAC:\t{}".format(mac()))
 
 print("Iface:\t{}".format(iface_name()))
 
-print("Route:\t{}".format(type(routing())))
+print("Route:")
+
+print(*routing().items(), sep="\n")
